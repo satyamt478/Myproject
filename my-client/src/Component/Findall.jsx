e61a3116eac8f2d6c7de9d8eb8109c63eeb9a1f6
satyamt478 <satyamtbsk21@gmail.com>
2025-07-28 13:57:18 +0530

    Update Findall.jsx

--- my-client/src/Component/Findall.jsx
+++ my-client/src/Component/Findall.jsx
@@ -10,7 +10,7 @@ export function FindAll() {
 
         try {
 
-            const response = await axios.get("http://localhost:3001/api/employees");
+            const response = await axios.get("https://satyam-employee.onrender.com/api/employees");
             setEmployees(response.data);
             //alert("Employees fetched successfully!");
 
